82170cc4ce39e0c05e0344ee4fa637e9ff68e76e
NikhilKalloli <1ms22cs091@msrit.edu>
2024-02-03 16:45:37 +0530

    Database Works on Render

--- index.js
+++ index.js
@@ -138,5 +138,5 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port: ${port}`);
 });
